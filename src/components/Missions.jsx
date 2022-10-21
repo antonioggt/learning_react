ca1dd6aa849ed671bee81c0c7f76948247196c3a
antonioggt <antonioggt85@gmail.com>
2022-10-21 17:40:28 -0300

    entrega o requisito 8

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
+    const hl = 'Missões';
     return (
-      <div data-testid="missions"> </div>
+      <div data-testid="missions">
+        <Title headline={ hl } />
+      </div>
     );
   }
 }
