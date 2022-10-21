053d91c8999eab839207b9bd44140926fab81495
antonioggt <antonioggt85@gmail.com>
2022-10-21 17:23:41 -0300

    resolve requisitos 5 e 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import Planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
+    const hl = 'Planetas';
     return (
       <div data-testid="solar-system">
-        <Title headline="Planetas" />
+        <Title headline={ hl } />
+        {Planets.map((e) => (
+          <PlanetCard key={ e.name } planetName={ e.name } planetImage={ e.image } />
+        ))}
       </div>
     );
   }
